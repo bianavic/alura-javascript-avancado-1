41a7570e7b33f63e5ae53ed767b63947427c73cd
bianavic <bianavic@gmail.com>
2019-09-07 14:14:53 -0300

    brincando com DOM

---
+++ js/oficio.js
@@ -0,0 +1,11 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Header"
+
+var oficio = document.querySelector("#primeiro-oficio")
+var tdOrigem = document.querySelector(".info-origem")
+
+var origem = tdOrigem.textContent
+
+console.log(oficio) // tr
+console.log(tdOrigem) // td
+console.log(origem) // 
